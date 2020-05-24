cc4fe3ef37765c27c8ae97b201f2b96b849687e6
james <jamespfennell@gmail.com>
2020-05-24 16:01:40 -0400

    update import

--- transiter/import_/importdriver.py
+++ transiter/import_/importdriver.py
@@ -331,11 +331,9 @@ class _Trip(parse.Trip):
             "route_pk": self.route_pk,
             "source_pk": self.source_pk,
             "direction_id": self.direction_id,
-            "start_time": self.start_time,
+            "started_at": self.start_time,
             "updated_at": self.updated_at,
-            "vehicle_id": self.train_id,
-            "current_status": self.current_status,
-            "current_stop_sequence": self.current_stop_sequence,
+            "delay": self.delay,
         }
         if self.is_new():
             del result["pk"]
