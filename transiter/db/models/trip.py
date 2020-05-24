5d2e42ecb792f236f6e7030a7df223e4bb8807c4
james <jamespfennell@gmail.com>
2020-05-24 15:40:24 -0400

    SQL Alchemy mapping bug fix

--- transiter/db/models/trip.py
+++ transiter/db/models/trip.py
@@ -32,7 +32,7 @@ class Trip(Base):
 
     source = relationship("FeedUpdate", cascade="none")
     route = relationship("Route", back_populates="trips", cascade="none")
-    vehicle = relationship("Vehicle", back_populates="vehicle", cascade="none")
+    vehicle = relationship("Vehicle", back_populates="trip", cascade="none")
     stop_times = relationship(
         "TripStopTime",
         back_populates="trip",
