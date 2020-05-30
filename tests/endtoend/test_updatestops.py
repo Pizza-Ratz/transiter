e386c44cedeb5e3b006968f0bd017f43bc215608
james <jamespfennell@gmail.com>
2020-05-30 10:20:44 -0400

    More end to end test cases

--- tests/endtoend/test_updatestops.py
+++ tests/endtoend/test_updatestops.py
@@ -1,149 +1,137 @@
+import uuid
+
 import pytest
 import requests
-
-from . import gtfsrealtimegenerator
-
-
-@pytest.mark.parametrize("num_steps", [6])
-def test_update_stops(install_system_1, transiter_host, source_server, num_steps):
-
-    __, realtime_feed_url = install_system_1("test_update_stops")
-
-    # (1)
-    trip_1_stops = {
-        "1AS": 300,
-        "1BS": 600,
-        "1CS": 800,
-        "1DS": 900,
-        "1ES": 1800,
-        "1FS": 2500,
-    }
-    feed_1 = gtfsrealtimegenerator.GtfsRealtimeFeed(
-        0, [gtfsrealtimegenerator.FeedTrip("trip_1", "A", trip_1_stops, 0)]
-    )
-    _perform_feed_update_stop_test(
-        transiter_host, source_server, realtime_feed_url, feed_1
-    )
-    if num_steps <= 1:
-        return
-
-    # (2) Change the stop times
-    trip_1_stops = {
-        "1AS": 200,
-        "1BS": 600,
-        "1CS": 800,
-        "1DS": 900,
-        "1ES": 1800,
-        "1FS": 2500,
-    }
-    feed_1 = gtfsrealtimegenerator.GtfsRealtimeFeed(
-        0, [gtfsrealtimegenerator.FeedTrip("trip_1", "A", trip_1_stops, 0)]
-    )
-    _perform_feed_update_stop_test(
-        transiter_host, source_server, realtime_feed_url, feed_1
-    )
-    if num_steps <= 2:
-        return
-
-    # (3) Add a new stop
-    trip_1_stops = {
-        "1AS": 200,
-        "1BS": 600,
-        "1CS": 800,
-        "1DS": 900,
-        "1ES": 1800,
-        "1FS": 2500,
-        "1GS": 2600,
-    }
-    feed_1 = gtfsrealtimegenerator.GtfsRealtimeFeed(
-        0, [gtfsrealtimegenerator.FeedTrip("trip_1", "A", trip_1_stops, 0)]
-    )
-    _perform_feed_update_stop_test(
-        transiter_host, source_server, realtime_feed_url, feed_1
-    )
-    if num_steps <= 3:
-        return
-
-    # (4) Delete a stop from the end
-    trip_1_stops = {"1AS": 200, "1BS": 600, "1CS": 800, "1DS": 900, "1ES": 1800}
-
-    feed_1 = gtfsrealtimegenerator.GtfsRealtimeFeed(
-        300, [gtfsrealtimegenerator.FeedTrip("trip_1", "A", trip_1_stops, 0)]
-    )
-    _perform_feed_update_stop_test(
-        transiter_host, source_server, realtime_feed_url, feed_1
-    )
-    if num_steps <= 4:
-        return
-
-    # (5) Add the stop back in ... probably don't need this case?
-    trip_1_stops = {
-        "1AS": 300,
-        "1BS": 600,
-        "1CS": 800,
-        "1DS": 900,
-        "1ES": 1800,
-        "1FS": 2500,
-        "1GS": 3000,
-    }
-    feed_1 = gtfsrealtimegenerator.GtfsRealtimeFeed(
-        850, [gtfsrealtimegenerator.FeedTrip("trip_1", "A", trip_1_stops, 0)]
-    )
-    _perform_feed_update_stop_test(
-        transiter_host, source_server, realtime_feed_url, feed_1
-    )
-    if num_steps <= 5:
-        return
-
-    # (6) Swap the ordering of the stops
-    trip_1_stops = {
-        "1AS": 300,
-        "1BS": 600,
-        "1CS": 800,
-        "1DS": 900,
-        "1ES": 1800,
-        "1GS": 2500,
-        "1FS": 3000,
-    }
-    feed_1 = gtfsrealtimegenerator.GtfsRealtimeFeed(
-        850, [gtfsrealtimegenerator.FeedTrip("trip_1", "A", trip_1_stops, 0)]
-    )
-    _perform_feed_update_stop_test(
-        transiter_host, source_server, realtime_feed_url, feed_1
-    )
+from google.transit import gtfs_realtime_pb2 as gtfs
 
 
-def _perform_feed_update_stop_test(
-    transiter_host, source_server, realtime_feed_url, feed_1
+@pytest.fixture
+def system_id(request):
+    return request.node.originalname + "__" + str(uuid.uuid4())
+
+
+TRIP_ID = "trip_id"
+ROUTE_ID = "A"
+TRIP_INITIAL_TIMETABLE = {
+    "1AS": 300,
+    "1BS": 600,
+    "1CS": 800,
+    "1DS": 900,
+    "1ES": 1800,
+    "1FS": 2500,
+}
+
+
+@pytest.mark.parametrize("use_stop_sequences", [True, False])
+@pytest.mark.parametrize("current_time", [0, 700, 4000])
+@pytest.mark.parametrize(
+    "stop_id_to_time_2",
+    [
+        # Basic case where the second update does nothing.
+        TRIP_INITIAL_TIMETABLE,
+        # Change the stop times
+        {"1AS": 200, "1BS": 600, "1CS": 800, "1DS": 900, "1ES": 1800, "1FS": 2500},
+        # Add a new stop
+        {
+            "1AS": 200,
+            "1BS": 600,
+            "1CS": 800,
+            "1DS": 900,
+            "1ES": 1800,
+            "1FS": 2500,
+            "1GS": 2600,
+        },
+        # Delete a stop from the end
+        {"1AS": 200, "1BS": 600, "1CS": 800, "1DS": 900, "1ES": 1800},
+        # Swap the ordering of the stops
+        {
+            "1AS": 300,
+            "1BS": 600,
+            "1CS": 800,
+            "1DS": 900,
+            "1ES": 1800,
+            "1GS": 2500,
+            "1FS": 3000,
+        },
+    ],
+)
+def test_trip__stop_view(
+    install_system_1,
+    system_id,
+    transiter_host,
+    source_server,
+    stop_id_to_time_2,
+    current_time,
+    use_stop_sequences,
 ):
-    source_server.put(realtime_feed_url, feed_1.build_feed())
 
-    requests.post(
-        transiter_host + "/systems/test_update_stops/feeds/GtfsRealtimeFeed?sync=true"
-    )
+    __, realtime_feed_url = install_system_1(system_id)
 
-    all_stop_data = feed_1.stop_data()
-    for stop_id, stop_data in all_stop_data.items():
-        actual_stop_data = []
+    stop_id_to_time_1 = TRIP_INITIAL_TIMETABLE
+
+    for stop_id_to_time in [stop_id_to_time_1, stop_id_to_time_2]:
+        source_server.put(
+            realtime_feed_url,
+            build_gtfs_rt_message(
+                0, stop_id_to_time, use_stop_sequences
+            ).SerializeToString(),
+        )
+        requests.post(
+            f"{transiter_host}/systems/{system_id}/feeds/GtfsRealtimeFeed?sync=true"
+        )
+
+    stop_id_to_stop_sequence = {
+        stop_id: stop_sequence + 25
+        for stop_sequence, stop_id in enumerate(stop_id_to_time_2.keys())
+    }
+    all_stop_ids = set(stop_id_to_time_1.keys()).union(stop_id_to_time_2.keys())
+    for stop_id in all_stop_ids:
+
+        # for stop_id, time in stop_id_to_time_2.items():
         response = requests.get(
-            transiter_host + "/systems/test_update_stops/stops/{}".format(stop_id)
+            f"{transiter_host}/systems/{system_id}/stops/{stop_id}"
         ).json()
-        for stu in response["stop_times"]:
-            actual_stop_data.append(
-                {
-                    "trip_id": stu["trip"]["id"],
-                    "route_id": stu["trip"]["route"]["id"],
-                    "arrival_time": stu["arrival"]["time"],
-                    "departure_time": stu["departure"]["time"],
-                }
-            )
-        assert stop_data == actual_stop_data
 
-    prev_stop_ids = set(all_stop_data.keys())
-    for stop_id in ["1AS", "1BS", "1CS", "1DS", "1ES", "1FS", "1GS"]:
-        if stop_id in prev_stop_ids:
+        time = stop_id_to_time_2.get(stop_id)
+        if time is None:
+            assert [] == response["stop_times"]
             continue
-        response = requests.get(
-            transiter_host + "/systems/test_update_stops/stops/{}".format(stop_id)
-        ).json()
-        assert [] == response["stop_times"]
+
+        stop_time = response["stop_times"][0]
+
+        assert stop_time["trip"]["id"] == TRIP_ID
+        assert stop_time["trip"]["route"]["id"] == ROUTE_ID
+        assert stop_time["arrival"]["time"] == time
+        assert stop_time["departure"]["time"] == time + 15
+        if use_stop_sequences:
+            assert stop_time["stop_sequence"] == stop_id_to_stop_sequence[stop_id]
+
+
+def build_gtfs_rt_message(current_time, stop_id_to_time, use_stop_sequences):
+    return gtfs.FeedMessage(
+        header=gtfs.FeedHeader(gtfs_realtime_version="2.0", timestamp=current_time),
+        entity=[
+            gtfs.FeedEntity(
+                id="1",
+                trip_update=gtfs.TripUpdate(
+                    trip=gtfs.TripDescriptor(
+                        trip_id=TRIP_ID, route_id=ROUTE_ID, direction_id=True
+                    ),
+                    stop_time_update=[
+                        gtfs.TripUpdate.StopTimeUpdate(
+                            arrival=gtfs.TripUpdate.StopTimeEvent(time=time),
+                            departure=gtfs.TripUpdate.StopTimeEvent(time=time + 15),
+                            stop_id=stop_id,
+                            stop_sequence=stop_sequence + 25
+                            if use_stop_sequences
+                            else None,
+                        )
+                        for stop_sequence, (stop_id, time) in enumerate(
+                            stop_id_to_time.items()
+                        )
+                        if time >= current_time
+                    ],
+                ),
+            )
+        ],
+    )
