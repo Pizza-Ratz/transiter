054e8ad9ade43033094e9196b7ea77b9ab218067
james <jamespfennell@gmail.com>
2021-03-21 18:56:15 -0400

    Better code

--- tests/performance/main.go
+++ tests/performance/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -12,31 +13,124 @@ import (
 
 const baseURL = "https://demo.transiter.dev"
 
-func main() {
+type Stats struct {
+	m     sync.Mutex
+	data  map[string][]time.Duration
+	nErrs int
+}
 
-	rand.Seed(303)
-	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func() {
-			var state State
-			var err error
-			state = StartState{baseUrl: baseURL}
-			for {
-				state, err = state.Transition()
-				if err != nil {
-					fmt.Println("Error:", err)
-				}
-				if _, ok := state.(EndState); ok {
-					break
-				}
-				time.Sleep(1 * time.Second)
-			}
-			wg.Done()
-		}()
-		time.Sleep(50 * time.Millisecond)
+func (stats *Stats) Add(id string, d time.Duration) {
+	stats.m.Lock()
+	defer stats.m.Unlock()
+	stats.data[id] = append(stats.data[id], d)
+}
+
+func (stats *Stats) AddErr() {
+	stats.m.Lock()
+	defer stats.m.Unlock()
+	stats.nErrs++
+}
+
+var stopStats Stats
+
+func main() {
+	stopStats = Stats{
+		data: map[string][]time.Duration{},
 	}
-	wg.Wait()
+	runWorkers()
+	for id, ds := range stopStats.data {
+		fmt.Println(id, ds)
+	}
+}
+
+func randDuration(lower, upper time.Duration) time.Duration {
+	return lower + time.Duration(float64(upper-lower)*rand.Float64())
+}
+
+func runWorkers() {
+	maxWorkers := 50
+	minSpawnDelay := 50 * time.Millisecond
+	maxSpawnDelay := 50 * time.Millisecond
+	minActionPause := 500 * time.Millisecond
+	maxActionPause := 500 * time.Millisecond
+
+	semaphore := NewSemaphore(maxWorkers)
+	ctx, cancelFunc := context.WithDeadline(context.Background(), time.Now().Add(50*time.Second))
+	defer cancelFunc()
+	t := time.NewTicker(time.Second)
+	defer t.Stop()
+	rand.Seed(303)
+	for {
+		select {
+		case <-semaphore.Acquire():
+			time.Sleep(randDuration(minSpawnDelay, maxSpawnDelay))
+			go func() {
+				var state State
+				var err error
+				state = StartState{baseUrl: baseURL}
+				for {
+					state, err = state.Transition()
+					if err != nil {
+						fmt.Println("Error:", err)
+					}
+					if _, ok := state.(EndState); ok {
+						break
+					}
+					time.Sleep(randDuration(minActionPause, maxActionPause))
+				}
+				semaphore.Release()
+			}()
+		case <-t.C:
+			fmt.Printf("Num workers: %d\n", semaphore.NumInUse())
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func NewSemaphore(size int) *Semaphore {
+	s := Semaphore{
+		c1:    make(chan struct{}),
+		c2:    make(chan struct{}, size-1),
+		nUsed: 0,
+	}
+	for i := 0; i < size-1; i++ {
+		s.c2 <- struct{}{}
+	}
+	go func() {
+		for {
+			s.c1 <- struct{}{}
+			s.m.Lock()
+			s.nUsed++
+			s.m.Unlock()
+			<-s.c2
+		}
+	}()
+	return &s
+}
+
+type Semaphore struct {
+	c1    chan struct{}
+	c2    chan struct{}
+	m     sync.Mutex
+	nUsed int
+}
+
+func (s *Semaphore) Acquire() <-chan struct{} {
+	return s.c1
+}
+
+func (s *Semaphore) Release() {
+	s.m.Lock()
+	s.nUsed--
+	s.m.Unlock()
+	s.c2 <- struct{}{}
+}
+
+func (s *Semaphore) NumInUse() int {
+	s.m.Lock()
+	defer s.m.Unlock()
+	return s.nUsed
 }
 
 type State interface {
@@ -81,10 +175,9 @@ func (state SystemState) Transition() (State, error) {
 	return routeState, nil
 }
 
-func NewRouteState(href string) (RouteState, error){
+func NewRouteState(href string) (RouteState, error) {
 	var routeState RouteState
-	d, err := get(href, &routeState.routeResponse)
-	fmt.Println("r", d)
+	_, err := get(href, &routeState.routeResponse)
 	return routeState, err
 }
 
@@ -121,7 +214,11 @@ func (state RouteState) selectServiceMap() int {
 func NewStopState(href string) (StopState, error) {
 	var stopState StopState
 	d, err := get(href, &stopState.stopResponse)
-	fmt.Println("s", d)
+	if err != nil {
+		stopStats.AddErr()
+	} else {
+		stopStats.Add(stopState.stopResponse.Id, d)
+	}
 	return stopState, err
 }
 
